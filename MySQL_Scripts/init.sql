f35cc4d8cfbe15a8bd7f78299f215ff2b6bb8812
mrtroll1 <luka.asfari@gmail.com>
2024-01-18 01:31:32 +0100

    Update init.sql

---
+++ MySQL_Scripts/init.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS user_cases;
+DROP TABLE IF EXISTS user_names;
+
+CREATE TABLE user_cases (
+    case_id INT AUTO_INCREMENT PRIMARY KEY,
+    case_name TEXT,
+    user_id BIGINT NOT NULL,
+    doctor_id BIGINT NOT NULL,
+    start_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    end_time DATETIME DEFAULT NULL,
+    status VARCHAR(20),
+    case_data TEXT,
+    FOREIGN KEY (user_id) REFERENCES user_names(user_id),
+    FOREIGN KEY (doctor_id) REFERENCES user_doctors(doctor_id)
+);
+
+
+CREATE TABLE user_names (
+    user_id BIGINT PRIMARY KEY,
+    user_name VARCHAR(50)
+);
+
+CREATE TABLE user_doctors (
+    user_id BIGINT PRIMARY KEY,
+    doctor_id BIGINT NOT NULL,
+    doctor_name VARCHAR(50)
+);
+
