3ffc7c50bcb106db69f8969b43699a7fc972e4c5
luka <luka.asfari@gmail.com>
2024-01-19 22:58:35 +0100

    Update init.sql

--- MySQL_Scripts/init.sql
+++ MySQL_Scripts/init.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS user_cases;
 DROP TABLE IF EXISTS user_doctors;
-DROP TABLE IF EXISTS user_names;
 DROP TABLE IF EXISTS users;
 
 
@@ -20,13 +19,11 @@ CREATE TABLE user_cases (
     case_id VARCHAR(64) PRIMARY KEY,
     case_name VARCHAR(64),
     user_id BIGINT NOT NULL,
-    doctor_id BIGINT DEFAULT 1,
     start_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     end_time DATETIME DEFAULT NULL,
     case_status VARCHAR(20),
     case_data TEXT,
     case_media_path VARCHAR(255) DEFAULT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (doctor_id) REFERENCES user_doctors(doctor_id)
 );
 
