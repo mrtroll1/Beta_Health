65e5f2cc6ee0f91c486b07b577c6ebc67b2c8005
luka <luka.asfari@gmail.com>
2024-01-29 16:44:17 +0100

    syntax

--- Python_files/scheduler_functions.py
+++ Python_files/scheduler_functions.py
@@ -12,9 +12,9 @@ async def send_scheduled_message(chat_id, message):
 
 async def schedule_message(chat_id, message, delay_in_hours=0, delay_in_minutes=0, delay_in_seconds=15):
     await bot.send_message(chat_id, 'entered schedule_message function')
-    schedule_time = datetime.datetime.now() + datetime.timedelta(seconds=delay_in_seconds, minutes=delay_in_minutes, hours=delay_in_hours)
+    scheduled_time = datetime.datetime.now() + datetime.timedelta(seconds=delay_in_seconds, minutes=delay_in_minutes, hours=delay_in_hours)
     await bot.send_message(chat_id, f'Отправка сообщения запланированна на {scheduled_time}')
-    scheduler.add_job(send_scheduled_message, 'date', run_date=schedule_time, args=[chat_id, message])
+    scheduler.add_job(send_scheduled_message, 'date', run_date=scheduled_time, args=[chat_id, message])
 
 
 
