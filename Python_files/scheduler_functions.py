67afa705d5ced642369c0556b16a35cfaedd2ef0
luka <luka.asfari@gmail.com>
2024-01-29 16:38:58 +0100

    syntax

--- Python_files/scheduler_functions.py
+++ Python_files/scheduler_functions.py
@@ -2,17 +2,18 @@ import apscheduler
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import datetime
 import asyncio
+from chat_bot import bot 
 
 scheduler = AsyncIOScheduler()
 scheduler.start()
 
-async def send_scheduled_message(bot, chat_id, message):
+async def send_scheduled_message(chat_id, message):
     await bot.send_message(chat_id, message)
 
-async def schedule_message(bot, chat_id, message, delay_in_hours=0, delay_in_minutes=0, delay_in_seconds=15):
+async def schedule_message(chat_id, message, delay_in_hours=0, delay_in_minutes=0, delay_in_seconds=15):
     await bot.send_message(chat_id, 'entered schedule_message function')
     schedule_time = datetime.datetime.now() + datetime.timedelta(seconds=delay_in_seconds, minutes=delay_in_minutes, hours=delay_in_hours)
-    scheduler.add_job(send_scheduled_message, 'date', run_date=schedule_time, args=[bot, chat_id, message])
+    scheduler.add_job(send_scheduled_message, 'date', run_date=schedule_time, args=[chat_id, message])
 
 
 
