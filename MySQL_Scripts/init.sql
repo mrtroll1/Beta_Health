42a8b8e7a1437854888131771b82b516b8606bcd
luka <luka.asfari@gmail.com>
2024-02-06 11:04:43 +0100

    sql syntax

--- MySQL_Scripts/init.sql
+++ MySQL_Scripts/init.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS user_cases;
-DROP TABLE IF EXISTS user_plans;
-DROP TABLE IF EXISTS user_doctors;
 DROP TABLE IF EXISTS user_documents;
+DROP TABLE IF EXISTS user_doctors;
+DROP TABLE IF EXISTS user_plans;
+DROP TABLE IF EXISTS user_cases;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -12,12 +12,18 @@ CREATE TABLE users (
     medical_bio TEXT DEFAULT NULL
 );
 
-CREATE TABLE user_documents (
-    document_tg_id BIGINT PRIMARY KEY,
-    document_name VARCHAR(100) DEFAULT 'No-name',
-    case_id BIGINT NOT NULL,
-    FOREIGN KEY (case_id) REFERENCES user_cases(case_id)
-)
+CREATE TABLE user_cases (
+    case_id VARCHAR(64) PRIMARY KEY,
+    case_name VARCHAR(64) DEFAULT 'No-name',
+    user_id BIGINT NOT NULL,
+    start_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    end_time DATETIME DEFAULT NULL,
+    case_status VARCHAR(20),
+    case_data TEXT DEFAULT NULL,
+    case_media_path VARCHAR(255) DEFAULT NULL,
+    case_media_tg_ids VARCHAR (255) DEFAULT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
 CREATE TABLE user_doctors (
     doctor_id BIGINT PRIMARY KEY,
@@ -34,17 +40,11 @@ CREATE TABLE user_plans (
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE user_cases (
-    case_id VARCHAR(64) PRIMARY KEY,
-    case_name VARCHAR(64) DEFAULT 'No-name',
-    user_id BIGINT NOT NULL,
-    start_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    end_time DATETIME DEFAULT NULL,
-    case_status VARCHAR(20),
-    case_data TEXT DEFAULT NULL,
-    case_media_path VARCHAR(255) DEFAULT NULL,
-    case_media_tg_ids VARCHAR (255) DEFAULT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+CREATE TABLE user_documents (
+    document_tg_id BIGINT PRIMARY KEY,
+    document_name VARCHAR(100) DEFAULT 'No-name',
+    case_id BIGINT NOT NULL,
+    FOREIGN KEY (case_id) REFERENCES user_cases(case_id)
 );
 
 
